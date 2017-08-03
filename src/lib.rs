e5e8a24ac881f7559ca55855d36db8b661b97369
Ian Jang <siabard@gmail.com>
2017-08-03 22:45:04 +0900

    search 함수 구성함

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments!");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>>{
+
+    // Reading file
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+   
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents) );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
-}
-
+use minigrep::Config;
 
 fn main() {
 
@@ -35,27 +19,14 @@ fn main() {
     println!("Query : {}", config.query);
     println!("FileName: {}", config.filename);
 
-    // if let Err(e) = run(config) {
+    // if let Err(e) = minigrep::run(config) {
     //     println!("Application error: {}", e);
 
     //     process::exit(1);
     // }
 
-    run(config).unwrap_or_else(|e| {
+    minigrep::run(config).unwrap_or_else(|e| {
         println!("Application error: {}", e);
         process::exit(1);
     });
-}
-
-fn run(config: Config) -> Result<(), Box<Error>>{
-
-    // Reading file
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("With text:\n{}", contents);
-   
-    Ok(())
 }
