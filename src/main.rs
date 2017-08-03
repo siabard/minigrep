76f6d2198403a8dff129e08f3064445ae1f69903
Ian Jang <siabard@gmail.com>
2017-08-03 21:58:52 +0900

    에러 메세지를 좀 더 예쁘게 만들었다. (panic 대신)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 struct Config {
     query: String,
@@ -8,11 +9,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments!");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
@@ -22,7 +25,10 @@ fn main() {
     // Accept command line arguments
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Query : {}", config.query);
     println!("FileName: {}", config.filename);
